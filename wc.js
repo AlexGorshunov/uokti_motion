a41c780ed2bdab71897b5c0ecd3a106a72f6c899
Alexander Shepel <al.iiieii@gmail.com>
2018-08-01 11:31:54 +0300

    Добавил датчик двери и кнопку

--- wc.js
+++ wc.js
@@ -31,29 +31,113 @@ aqara.on('gateway', (gateway) => {
     console.log(`  Type: ${device.getType()}`);
     console.log(`  SID: ${device.getSid()}`);
     switch (device.getType()) {
-      case 'motion':
-        console.log(`  Motion (${device.hasMotion() ? 'motion' : 'no motion'})`);
-        device.on('motion', () => {
-          console.log(`${device.getSid()} has motion${device.getLux() !== null ? ' (lux:' + device.getLux() + ')' : ''}`)
-          request.post(`wc/motion`, {
+      case 'magnet':
+        console.log(`  Magnet (${device.isOpen() ? 'open' : 'close'})`);
+        device.on('open', () => {
+          console.log(`${device.getSid()} is now open`);
+          request.post(`wc/_doc`, {
             body: {
               '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
               device: {
-                sid: device.getSid(),
-                lux: device.getLux()
+                type: device.getType(),
+                sid: device.getSid()
+              },
+              magnet: 'open'
+            }
+          }).catch(console.error)
+        });
+        device.on('close', () => {
+          console.log(`${device.getSid()} is now close`);
+          request.post(`wc/_doc`, {
+            body: {
+              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              device: {
+                type: device.getType(),
+                sid: device.getSid()
+              },
+              magnet: 'close'
+            }
+          }).catch(console.error)
+        });
+        break;
+      case 'switch':
+        console.log(`  Switch`);
+        device.on('click', () => {
+          console.log(`${device.getSid()} is clicked`);
+          request.post(`wc/_doc`, {
+            body: {
+              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              device: {
+                type: device.getType(),
+                sid: device.getSid()
+              },
+              magnet: 'click'
+            }
+          }).catch(console.error)
+        });
+        device.on('doubleClick', () => {
+          console.log(`${device.getSid()} is double clicked`);
+          request.post(`wc/_doc`, {
+            body: {
+              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              device: {
+                type: device.getType(),
+                sid: device.getSid()
+              },
+              magnet: 'doubleClick'
+            }
+          }).catch(console.error)
+        });
+        device.on('longClickPress', () => {
+          console.log(`${device.getSid()} is long pressed`);
+          request.post(`wc/_doc`, {
+            body: {
+              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              device: {
+                type: device.getType(),
+                sid: device.getSid()
+              },
+              magnet: 'longClickPress'
+            }
+          }).catch(console.error)
+        });
+        device.on('longClickRelease', () => {
+          console.log(`${device.getSid()} is long released`);
+          request.post(`wc/_doc`, {
+            body: {
+              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              device: {
+                type: device.getType(),
+                sid: device.getSid()
+              },
+              magnet: 'longClickRelease'
+            }
+          }).catch(console.error)
+        });
+        break;
+      case 'motion':
+        console.log(`  Motion (${device.hasMotion() ? 'motion' : 'no motion'})`);
+        device.on('motion', () => {
+          console.log(`${device.getSid()} has motion${device.getLux() !== null ? ' (lux:' + device.getLux() + ')' : ''}`);
+          request.post(`wc/_doc`, {
+            body: {
+              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              device: {
+                type: device.getType(),
+                sid: device.getSid()
               },
               motion: true
             }
           }).catch(console.error)
         });
         device.on('noMotion', () => {
-          console.log(`${device.getSid()} has no motion (inactive:${device.getSecondsSinceMotion()}${device.getLux() !== null ? ' lux:' + device.getLux() : ''})`)
-          request.post(`wc/motion`, {
+          console.log(`${device.getSid()} has no motion (inactive:${device.getSecondsSinceMotion()}${device.getLux() !== null ? ' lux:' + device.getLux() : ''})`);
+          request.post(`wc/_doc`, {
             body: {
               '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
               device: {
-                sid: device.getSid(),
-                lux: device.getLux()
+                type: device.getType(),
+                sid: device.getSid()
               },
               secondsSinceMotion: device.getSecondsSinceMotion(),
               motion: false
