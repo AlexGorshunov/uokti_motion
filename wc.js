871914b6ec38d6865ab17d339f2442690afa4147
Alexander Shepel <al.iiieii@gmail.com>
2018-08-01 12:34:04 +0300

    Добавил миллисекунды

--- wc.js
+++ wc.js
@@ -37,7 +37,7 @@ aqara.on('gateway', (gateway) => {
           console.log(`${device.getSid()} is now open`);
           request.post(`wc/_doc`, {
             body: {
-              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
               device: {
                 type: device.getType(),
                 sid: device.getSid()
@@ -50,7 +50,7 @@ aqara.on('gateway', (gateway) => {
           console.log(`${device.getSid()} is now close`);
           request.post(`wc/_doc`, {
             body: {
-              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
               device: {
                 type: device.getType(),
                 sid: device.getSid()
@@ -66,7 +66,7 @@ aqara.on('gateway', (gateway) => {
           console.log(`${device.getSid()} is clicked`);
           request.post(`wc/_doc`, {
             body: {
-              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
               device: {
                 type: device.getType(),
                 sid: device.getSid()
@@ -79,7 +79,7 @@ aqara.on('gateway', (gateway) => {
           console.log(`${device.getSid()} is double clicked`);
           request.post(`wc/_doc`, {
             body: {
-              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
               device: {
                 type: device.getType(),
                 sid: device.getSid()
@@ -92,7 +92,7 @@ aqara.on('gateway', (gateway) => {
           console.log(`${device.getSid()} is long pressed`);
           request.post(`wc/_doc`, {
             body: {
-              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
               device: {
                 type: device.getType(),
                 sid: device.getSid()
@@ -105,7 +105,7 @@ aqara.on('gateway', (gateway) => {
           console.log(`${device.getSid()} is long released`);
           request.post(`wc/_doc`, {
             body: {
-              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
               device: {
                 type: device.getType(),
                 sid: device.getSid()
@@ -121,7 +121,7 @@ aqara.on('gateway', (gateway) => {
           console.log(`${device.getSid()} has motion${device.getLux() !== null ? ' (lux:' + device.getLux() + ')' : ''}`);
           request.post(`wc/_doc`, {
             body: {
-              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
               device: {
                 type: device.getType(),
                 sid: device.getSid()
@@ -134,7 +134,7 @@ aqara.on('gateway', (gateway) => {
           console.log(`${device.getSid()} has no motion (inactive:${device.getSecondsSinceMotion()}${device.getLux() !== null ? ' lux:' + device.getLux() : ''})`);
           request.post(`wc/_doc`, {
             body: {
-              '@timestamp': moment().utcOffset('+03:00').format(moment.defaultFormat),
+              '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
               device: {
                 type: device.getType(),
                 sid: device.getSid()
